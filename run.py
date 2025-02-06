0868e8f0eabcae7aeaa06b7487db1e045db6196b
Clarence <agccclarence@gmail.com>
2025-02-06 20:37:05 +0800

    changes to config1

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app, db
 
 if __name__ == '__main__':
-    db.create_all()  # Create database tables
+    with app.app_context():
+        db.create_all()  # Create database tables
     app.run(debug=True)
