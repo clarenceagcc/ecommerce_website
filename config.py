ee3dfd98bd5f2e8f0105fda5a5ab1b79a8293c09
Clarence <agccclarence@gmail.com>
2025-02-06 21:01:27 +0800

    maybe1

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:testtest@db-project.ceaomtz44zvm.us-east-1.rds.amazonaws.com:3306/ecommerce_db?auth_plugin=mysql_native_password'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:testtest@db-project.ceaomtz44zvm.us-east-1.rds.amazonaws.com:3306/ecommerce_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
