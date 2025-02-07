a629535e6957d5b927f567f336f6d6e0e24f9f75
Clarence <agccclarence@gmail.com>
2025-02-07 15:05:05 +0800

    dqw

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///ecommerce.db'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:testtest@db-project.ceaomtz44zvm.us-east-1.rds.amazonaws.com:3306/ecommerce_db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///ecommerce.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:testtest@db-project.ceaomtz44zvm.us-east-1.rds.amazonaws.com:3306/ecommerce_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
